0eca6e6fa4dc5cb46ea59b8b3597d41e082d450e
julepka <julepka@gmail.com>
2021-03-11 19:02:50 +0200

    fixing versioning

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "themis",
-                      url: "https://github.com/julepka/themis/releases/download/2.1/themis.xcframework.zip",
+                      url: "https://github.com/julepka/themis/releases/download/2.2.0/themis.xcframework.zip",
                       // $swift package compute-checksum output/themis.xcframework.zip
                       checksum: "b4a8d8868cbd09499d75a10315d1231281d40dee11d1fe549cf4893967f3ee38"),
 
