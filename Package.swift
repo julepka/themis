e68d10fe58c9a170b437d189d25f633c6ec3d080
julepka <julepka@gmail.com>
2021-03-11 18:03:52 +0200

    fixing spn

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "themis",
-                      url: "https://github.com/julepka/themis/releases/download/0.15.1/themis.xcframework.zip",
+                      url: "https://github.com/julepka/themis/releases/download/2.0/themis.xcframework.zip",
                       // $swift package compute-checksum output/themis.xcframework.zip
                       checksum: "d894fc82df1fbf7542afca0971455a37e843888519bf355ba757bbfef1137a77"),
 
